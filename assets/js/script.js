0d58d5b8424c068bc9fc751323632d90a8b482a2
allapetr <69552018+allapetr@users.noreply.github.com>
2021-04-01 19:29:55 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,14 @@
                 required: true,
                 minlength: 3
             },
+            age: {
+                required: true,
+                minlength: 1
+            },
+            country: {
+                required: true,
+                minlength: 2
+            },
             email: {
                 required: true,
                 email: true
@@ -23,6 +31,14 @@
             name: {
                 required: "Please type your name",
                 minlength: "Your name must consist of at least 2 characters"
+            },
+            age: {
+                required: "Please type your age",
+                minlength: "Your age must consist of at least 1 character"
+            },
+            country: {
+                required: "Please type your Country of Residence (State/City)",
+                minlength: "It must consist of at least 2 characters"
             },
             email: {
                 required: "Please type your email address",
